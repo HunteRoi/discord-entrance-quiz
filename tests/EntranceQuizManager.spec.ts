54349e4d5a8bf3068e067fcfc48b76cd2d66dfae
HunteRoi <me@tinaeldevresse.eu>
2022-05-27 19:32:20 +0200

    TCR

--- tests/EntranceQuizManager.spec.ts
+++ tests/EntranceQuizManager.spec.ts
@@ -4,11 +4,13 @@ import { EntranceQuizManager } from '../src/EntranceQuizManager';
 
 describe('EntranceQuizManager', () => {
     let client: Client;
+    let questions: any[];
 
     beforeEach(() => {
         client = new Client({ intents: [Intents.FLAGS.GUILDS] });
         client.removeAllListeners();
 
+        questions = [];
     });
 
     it('should throw an error when client is null', () => {
@@ -17,5 +19,9 @@ describe('EntranceQuizManager', () => {
 
     it('should throw an error when the quiz questions are not provided', () => {
         expect(() => new EntranceQuizManager(client, null)).toThrowError('You must provide questions!');
-    })
+    });
+
+    it('should not throw', () => {
+        expect(() => new EntranceQuizManager(client, questions)).not.toThrow();
+    });
 });
